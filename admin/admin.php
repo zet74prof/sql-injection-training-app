7150264eb7f2605f55a0bcab56bfdd3a06883f8c
Zet74 <etienne.buffet@gmail.com>
2025-05-14 14:40:07 +0200

    Change to PDO instead of MSQLI

---
+++ admin/admin.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Admin</h1>";
+echo "<p>Welcome to the admin page. You can reset the database here.</p>";
+echo "<p>Click <a href='resetdb.php'>here</a> to reset the database.</p>";
+echo "<p>Click <a href='index.php'>here</a> to go back to the home page.</p>";
