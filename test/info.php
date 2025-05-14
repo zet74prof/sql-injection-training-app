b3ef8ac992ed0726252a06b69c01b41045effecd
Zet74 <etienne.buffet@gmail.com>
2025-05-14 14:18:47 +0200

    Change to PDO instead of MSQLI

---
+++ test/info.php
@@ -0,0 +1,14 @@
+<?php
+# Sensitive Data Example (Demo)
+
+//This file demonstrates how sensitive data might look in a configuration file. **This is fake data and should never be used in production.**
+
+## Example Configuration
+
+/*
+```plaintext
+DBUSER: sqlitraining
+DBPASS: &6HAUTdanslaFauré
+DBHOST: 127.0.0.1
+DBNAME: sqlitraining
+*/
